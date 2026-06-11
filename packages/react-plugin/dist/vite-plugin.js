afb8e7f02726573dbd04ef930edfe778a87c6c5c
Tom Sowerby <twsowerby@gmail.com>
2026-06-11 17:54:51 +1000

    chore: gitignore dist/ and test-app build artifacts

--- packages/react-plugin/dist/vite-plugin.js
+++
@@ -1,9 +0,0 @@
-import {
-  piDesignVitePlugin
-} from "./chunk-HZYNPMN2.js";
-import "./chunk-4ZKYYSAG.js";
-import "./chunk-P5LJJ4GM.js";
-export {
-  piDesignVitePlugin
-};
-//# sourceMappingURL=vite-plugin.js.map
